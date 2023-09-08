eb173308834f0313226c448e51022b958eddb183
João <povoajoao6@gmail.com>
2023-09-08 15:53:44 -0300

    improving and creating a funtion to add a word to an array

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-let word = "feijaoMexido";
+let list = [];
 
-for(let i=0; i < word.length; i++) {
-    console.log(i);
+
+const createAlphabet = (word) => {
+    if (list.includes(word)) {
+        console.log('That word already exists');
+    } else {
+        let isEqual = false;
+
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] === word) {
+                isEqual = true;
+                break;
+            }
+        }
+
+        if (isEqual) {
+            console.log('That word already exists');
+        } else {
+            list.push(word);
+            console.log('Added with success');
+        }
+    }
 }
